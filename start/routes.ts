a9f09ef71297c6ccb02a99159e0ac475a5c1a42e
Dolu <doludev@protonmail.com>
2021-06-16 17:18:46 +0200

    Fix postbuild command

--- start/routes.ts
+++ start/routes.ts
@@ -21,7 +21,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async ({ view }) => {
-	const specUrl = '/docs/swagger.json'
+	const specUrl = '/swagger.json'
 	return view.render('swagger', { specUrl })
 })
 
